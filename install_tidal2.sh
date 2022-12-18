ade04e449c6d3da0e4a14c965437d0b310b2905a
Nicolas Bonamy <nicolas@bonamy.fr>
2022-12-18 07:28:56 -0600

    cleanup

--- install_tidal2.sh
+++ install_tidal2.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # dependencies
-sudo apt install libncursesw5 screen
+apt install libncursesw5 screen
 
 # speaker contol service
-sudo ln -s /home/mnmt/pi-fi/tidal-speaker_controller.service /lib/systemd/system/
+ln -sf /home/mnmt/pi-fi/tidal-speaker_controller.service /lib/systemd/system/
 
 # enable on boot
-sudo systemctl enable tidal-speaker_controller.service
+systemctl enable tidal-speaker_controller.service
 
 # start service and check on status
 systemctl daemon-reload
