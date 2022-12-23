87164bf542160e2ef62a45dff893f5821328ca56
Nicolas Bonamy <nicolas@bonamy.fr>
2022-12-22 22:38:08 -0600

    airplay install fix

--- install_airport.sh
+++ install_airport.sh
@@ -7,24 +7,41 @@
 # install required packages
 apt install -y autoconf libtool libdaemon-dev libasound2-dev libpopt-dev libconfig-dev
 apt install -y avahi-daemon libavahi-client-dev
-apt install -y libssl-dev
+apt install -y libssl-dev libplist-dev libsodium-dev libgcrypt-dev
 
-# git clone repository
+# nqptp
+cd ~
+git clone https://github.com/mikebrady/nqptp.git
+cd nqptp/
+
+# prepare build
+autoreconf -fi
+./configure --with-systemd-startup
+
+# now build and deploy
+make
+make install
+
+# enable and start service
+systemctl enable nqptp
+systemctl start nqptp
+
+# shairport now
 cd ~
 git clone https://github.com/mikebrady/shairport-sync.git
 
 # prepare build
 cd shairport-sync
 autoreconf -i -f
-./configure --with-alsa --with-avahi --with-ssl=openssl --with-systemd --with-metadata
+./configure --with-alsa --with-avahi --with-ssl=openssl --with-systemd --with-metadata --with-airplay-2
 
 # now build and deploy
 make
 make install
 
-# enable service
+# overwrite config
+ln -sf /home/mnmt/pi-fi/shairport-sync.conf /usr/local/etc/
+
+# enable and start service
 systemctl enable shairport-sync
-
-# start service
 service shairport-sync start
-
